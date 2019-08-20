4bf9f5766c46f1d0b6923a9f3d5646139855edbf
hamza mugabo <hamzamugabo3@gmail.com>
2019-08-20 17:29:20 +0300

    display blog comments

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -22,30 +22,31 @@
                         Posted
                         {{$blog->created_at->diffForHumans()}}
 
-                    @foreach($blog->comments as $comment)
+                    @foreach($comments as $comment)
 
 
                     {{$comment->comment}}
 
 
                     @endforeach
-                    <form action="{{route('create_comment')}}"  enctype="multipart/form-data">
-                        @csrf
+                    {{--<form action="{{route('store_comment')}}" method="POST"  enctype="multipart/form-data">--}}
+                        {{--@csrf--}}
                         {{--@method('POST')--}}
-                        <div class="form-group">
-                            <label for="title">Comment</label>
-                            <textarea name="comment" rows="3" class="form-control"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-outline-primary"> Add Comment
+                        {{--<div class="form-group">--}}
+                            {{--<label for="title">Comment</label>--}}
+                            {{--<textarea name="comment" rows="3" class="form-control"></textarea>--}}
+                        {{--</div>--}}
+                        {{--<div class="form-group">--}}
+                            {{--<button type="submit" class="btn btn-outline-primary"> Add Comment--}}
 
-                            </button>
-                        </div>
-                    </form>
+                            {{--</button>--}}
+                        {{--</div>--}}
+                    {{--</form>--}}
 
                     </p>
 
                     <a href="{{route('blog_path',['blog'=>$blog->id])}}" class="btn btn-outline-primary">View Post</a>
+                    <a href="{{route('comments_path')}}" class="btn btn-outline-primary">View Comments</a>
                 </div>
             </div>
             @endforeach
